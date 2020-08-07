5d4b7d1bbed2166f6dbbc20d831a3fda45d01fdd
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-08-07 12:20:21 +0200

    Create documentation1.java

---
+++ APL2.1/heritage/documentation1.java
@@ -0,0 +1,21 @@
+
+public class documentation1{
+
+
+
+	public static void main(String[] args) {
+
+		//boucle qui prend les arguements 
+		for(int i=0; i<args.length; i++){
+			
+			// mise des majuscules sur les arguements
+			args[i] = args[i].toUpperCase();
+			// affichage
+			System.out.println(args[i]);
+			
+		}
+
+	}
+}
+
+
