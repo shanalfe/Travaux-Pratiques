730c53940d4d8c48796b763c019ef18748f0aeb4
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-08-07 12:24:49 +0200

    Create Moto.java

---
+++ APL2.1/polymorphismes/vehicules/Moto.java
@@ -0,0 +1,15 @@
+
+// on utilise implements car hérite d'une interface
+public class Moto implements  Vehicule  {
+	//déclaration
+	int nbRoues =2;
+
+	public String sorte(){
+		return "Moto";
+	}
+
+	public int nbRoues(){
+		return nbRoues;
+	}
+	
+}
