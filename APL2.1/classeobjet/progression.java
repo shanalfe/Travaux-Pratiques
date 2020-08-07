68518f8dac117b818e012ac614cbfe1a5b00c4ff
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-08-07 12:09:23 +0200

    Add files via upload

---
+++ APL2.1/classeobjet/progression.java
@@ -0,0 +1,22 @@
+
+public class progression {
+	public static void main(String[] args) {
+		Compteur compte = new Compteur();
+		
+		for(int i=0; i<5; i++){
+			compte.plusUn();
+		}
+
+	System.out.println(compte.toString());
+
+	for(int i=0; i<4; i++){
+		compte.plusUn();
+		
+	}
+	System.out.println(compte.toString());
+
+}
+}
+	
+
+
