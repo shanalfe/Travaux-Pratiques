cd687955756cfb2afbfa7bd20424059194738dc6
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-08-07 12:23:09 +0200

    Create moyenne.java

---
+++ APL2.1/moyenne/moyenne.java
@@ -0,0 +1,15 @@
+
+import java.io.*; 
+import java.util.*
+import java.util.ArrayList;
+
+public class Moyenne {
+	public static void main(String[] args) {
+
+		Collection <String> moy = new LinkedList<String>
+
+		moy.add();
+
+		
+	}
+}
