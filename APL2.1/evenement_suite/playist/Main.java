284f6d6810b3c8f426f743934ef933c8d69bab2c
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-08-07 12:17:40 +0200

    Create Main.java

---
+++ APL2.1/evenement_suite/playist/Main.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Main {
+	public static void main(String[] args) {
+		
+		JFrame fenetre = new JFrame();
+		fenetre.setSize(500,500);
+		fenetre.setLocation(0,0);
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+
+
+
+		//affichage de la fenetre
+		fenetre.setVisible(true);
+	}
+}
