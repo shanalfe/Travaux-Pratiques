59256f492d2c46d1e9d428145a589e0e489b0472
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-08-07 12:22:03 +0200

    Create choix.java

---
+++ APL2.1/miseenpage/choix.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.ButtonGroup;
+
+public class choix {
+	public static void main(String[] args) {
+		
+		JFrame fenetre = new JFrame();
+
+		fenetre.setSize(800,400);
+		fenetre.setLocation(0,0);
+
+		ButtonGroup group = new ButtonGroup();
+		group.add(JRadioButton "tets");
+		group.add(JRadioButton "nc");
+
+		fenetre.setVisible(true);
+	}
+}
