6e575fc24a7f926a473589a579c1c57fa800ae92
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-09-07 12:12:37 +0200

    Add files via upload

--- APL1.1/condition/triple.c
+++ APL1.1/condition/triple.c
@@ -1,32 +1,25 @@
-#include<stdlib.h>
 #include<stdio.h>
+#include<stdlib.h>
 
-
-int main(int argc, char const *argv[])
-{
-	int entier, nbre;
-	int multiple;
+int main(int argc, char * argv[]){
+	int multiple,actual,result;
 	
-	printf("Entrez un entier naturel :\n");
-	
-	// resulat vaut l'entier rentré par l'utilisateur
-	nbre = scanf("%d", &entier);
+	printf("Entrez un entier naturel : ");
+	result = scanf("%d",&actual);
 
-
-	if (nbre%3 == 3){
-		printf("%d est un multiple de 3\n", entier );
-	}else{
-		if(nbre%3 == 1){
-			multiple = entier - 1;
-			printf("%d n'est pas un multiple de 3, mais %d est le nombre le plus proche\n",entier, multiple );
-
-		}else{
-			multiple= entier + 1;
-			printf("%d n'est pas un multiple de 3, mais %d est le nombre le plus proche\n",entier, multiple );
-		}
+	if(result != 1){
+		printf("ce n'est pas un entier\n");
+		return EXIT_FAILURE;
 	}
-
-
-
+	if(actual%3 == 0){
+		printf("%d est un multiple de 3\n",actual);
+	} else if(actual%3 == 1){
+		multiple = actual -1;
+		printf("%d n'est pas multiple de 3 mais %d est le multiple de 3 le plus proche\n",actual,multiple);
+	}	else {
+		multiple = actual + 1;
+		printf("%d n'est pas multiple de 3 mais %d est le multiple de 3 le plus proche\n",actual,multiple);
+	}
 	return EXIT_SUCCESS;
 }
+
