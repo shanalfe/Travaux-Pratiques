a5b861da25c375750110d01412d41cb20b10dfb7
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-09-09 15:54:19 +0200

    Update monnaie.c

--- APL1.1/type/monnaie.c
+++ APL1.1/type/monnaie.c
@@ -1,3 +1,5 @@
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
@@ -5,12 +7,33 @@
 
 int main(int argc, char const *argv[])
 {
-  float sommeClient;
+	int pieceA=0, pieceB=0, pieceC=0;
 
+	double sommeClient = 0.00;
+	
   printf("Vous devez 5,49 euros. Quelle est le montant de votre somme ?\n");  
   scanf("%lf", &sommeClient);
 
-  printf(" somme donnée : %lf\n", sommeClient );
+
+  sommeClient = sommeClient - 5,49;
+      printf("On vous rend %lf euros\n", sommeClient );
+
+  if (sommeClient<0)
+  {
+  	printf("Pas assez de sous...\n");
+  }else if (sommeClient>0){
+  	for (pieceA=0; sommeClient - 2 > 0; ++pieceA){
+  		sommeClient= sommeClient-2;
+  	}
+  	for (pieceB = 0; sommeClient - 0.20 >=0; ++pieceB){
+  		sommeClient= sommeClient-0.20;
+  	}
+  	for (pieceC = 0; sommeClient-0.01>=0; ++pieceC){
+  		sommeClient=sommeClient-0.01;
+  	}
+  }
+
+  printf("On vous a rendu %d pièce(s) de 2 euros, %d pièce(s) de 20 centimes et %d pièce(s) de 1 centime \n",pieceA, pieceB, pieceC );
     
     return EXIT_SUCCESS;
 }
