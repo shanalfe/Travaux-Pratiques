5c3dbee1cd694c72c74c2623092935d52bde8536
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-09-15 15:17:57 +0200

    Create chiffre.c

---
+++ APL1.1/type/chiffre.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+
+int main()
+{
+
+int nombre;
+int c, d, u;
+
+printf("Entrez un entier \n");
+scanf("%d", &nombre);
+
+c = nombre / 100;
+d = (nombre - c *100) /10;
+u = nombre - (c * 100 + d * 10);
+
+printf("%d est la centaine, %d est la dizaine, %d est l'unité", c, d, u);
+
+
+
+    return EXIT_SUCCESS;
+}
