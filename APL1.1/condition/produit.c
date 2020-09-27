dffb2b9c7883df27ff4e6a6962cf82a1e4617614
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-09-27 21:46:07 +0200

    Update produit.c

--- APL1.1/condition/produit.c
+++ APL1.1/condition/produit.c
@@ -1,19 +1,29 @@
-#include<stdlib.h>
 #include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char * argv[]){
+
+int a, b;
+
+	printf("Entrez deux entiers : \n");
+	printf("premier entier :\n");
+	scanf("%d", &a);
+
+	printf("le deuxième:\n");
+	scanf("%d", &b);
+	
+	if (a == 0 || b== 0){
+		printf("le produit est nul\n");
+	}
+	if (a<0 && b<0 || a>0 && b>0){
+		printf("Le produit est positif\n");
+	} 
+ 	if(a>0 && b<0 || a<0 && b>0){
+		printf("Le produit est négartif\n");
+	}
+
+	return EXIT_SUCCESS;
 
 
-int main(int argc, char const *argv[])
-{
 
-int first;
-int second;
-
-printf("entrez un premier entier :\n");
-scanf("%d", &first);
-
-printf("entrez un second entier\n");
-scanf("%d", &second);
-
-printf("%d x %d \n",first, second );
-	return 0;
 }
