cd1b758f2277e03ceb01a42fdc0f6fa678ef3b11
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-09-27 19:14:44 +0200

    Update figure.c

--- APL1.1/boucle-encore/figure.c
+++ APL1.1/boucle-encore/figure.c
@@ -1,87 +1,50 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
-
-
-int main(int argc, char const *argv[])
+int main()
 {
+    int result, choix;
+    int hauteur, i, j;
 
-	// déclaration des variables
-	int choix;
+    while (choix != 3){
+    printf("\n");
+    printf("1)Triangle \n 2) Carré \n q) Quitter \n");
+    result = scanf("%d", &choix);
 
-		//variables triangle
-		int hauteurtriangle;
-		int i,j;
+    if (result == 3) {
+        printf("Au revoir ! \n");
+        return EXIT_FAILURE;
+    }
 
-	
-		//variables carré
-		int hauteurcarre;
-		int k,l;
-		
+    if (choix == 1){
+        printf("hauteur ? \n");
+        scanf("%d", &hauteur);
+        printf("*");
 
-		//variable menu
-		int a,b; 
+        for(i= 0; i<=hauteur; i++){
+            printf("*");
+            printf("");
+        }
+    }
 
-
-	
-	//boucle répétition du menu
-	for(a=0; a<b; a++){
-
-	//choix
-	printf("Choissisez parmi les propositions suivantes :\n");
-	printf("1) Triangle\n 2) Carré \n 3) quitter \n");
-	scanf("%d", &choix);
-
-
-	// triangle 
-	if(choix==1){
-		printf("Hauteur ?\n");
-		scanf("%d", &hauteurtriangle);
-
-			//boucle hauteur 
-			for(i=0; i<hauteurtriangle; i++){
-				// boucle ligne 
-				for(j=0; j<hauteurtriangle; j++){
-					printf("*");
-				}
-
-				printf("\n");
-
-			}
+    if (choix == 2){
+      for (i=0;i < hauteur;i++){
+		for(j=0;j < hauteur-i;j++){
+			printf(" ");
+		}
+		printf("*");
+		for(j=0;j<i;j++){
+			printf("**");
+		}
+		printf("\n");
 	}
+    }
 
 
 
-	//carré
-	if(choix == 2){
-		printf("Hauteur ?\n");
-		scanf("%d", &hauteurcarre);
-
-			for(k=0; k<=hauteurcarre; k++){
-				printf("*");
-
-				for(l=0; l<hauteurcarre-1; l++){
-					printf("*");					
-				}
-				printf("\n");
-			}
+    }
 
 
-
-	printf("\n");
-	}
-
-
-
-
-	//quitter
-	if(choix ==3){
-		printf("Au revoir\n");
-		return EXIT_SUCCESS; // fin du programme
-	}
-
-}
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
