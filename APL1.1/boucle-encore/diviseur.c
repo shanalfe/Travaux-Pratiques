d04de8c72d969a395b8ef00cb1464380429f135f
shana <shanoulefevre@orange.fr>
2020-10-05 10:52:32 +0200

    ajout

---
+++ APL1.1/boucle-encore/diviseur.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	int a, b;
+
+	printf("Entrez deux entiers naturels : \n");
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+
+	return 0;
+}
