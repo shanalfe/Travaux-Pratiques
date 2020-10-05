024da782e475a96901dd36c6949ea3bf87ba6328
shana <shanoulefevre@orange.fr>
2020-10-05 16:25:18 +0200

    maj

---
+++ APL1.1/ControleAPL1.1/constantes.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	
+	char a = '9';
+	long b = 9L;
+	long double c = 9.0L;
+	int d = 0x9;
+	
+
+	printf("%c\n", a);
+	printf("%ld\n",b);
+	printf("%1.0LF\n", c);
+	printf("%d\n",d );
+
+
+
+	return 0;
+}
