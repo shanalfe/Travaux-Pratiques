1cbd610927b85c4f6604c8eb34c15c88ca70ef1a
shana <shanoulefevre@orange.fr>
2020-10-05 10:48:09 +0200

    Test programme controle APL

---
+++ APL1.1/ProgrammeTest/exo1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	int k = 100;
+	int r = k%3;
+
+	scanf("%d", &k);
+	printf("%d %d\n",k,r );
+	return 0;
+}
---
+++ APL1.1/ProgrammeTest/exo2.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int i = 10;
+	int j= 8;
+	int k = 0;
+	{
+		int i=j;
+		int j= i+1;
+		printf("%d %d,%d\n",i,j,k );
+		k=j;
+	}
+	return 0;
+}
