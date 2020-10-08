451a9065d16968f114ba3d9e3720d45e0f96990a
shana <shanoulefevre@orange.fr>
2020-10-08 18:30:29 +0200

    envoi de diviseur.c

--- APL1.1/boucle-encore/diviseur.c
+++ APL1.1/boucle-encore/diviseur.c
@@ -3,12 +3,19 @@
 
 int main(int argc, char const *argv[])
 {
-	int a, b;
+	    int a, b, p, i;
+  
+    printf("Entrez deux entiers: ");
+    scanf("%d %d", &a, &b);
+  
+    for(i=1; (i <= a) && (i <= b); ++i)
+    {
+        if( (a%i==0) && (b%i==0) ){ //PGCD: diviser 2 nombres sans reste
+            p = i;
+        }
+    }
+  
+    printf("Le PGCD de %d et %d = %d \n", a, b, p);
 
-	printf("Entrez deux entiers naturels : \n");
-	scanf("%d", &a);
-	scanf("%d", &b);
-
-
-	return 0;
+    return EXIT_SUCCESS;
 }
