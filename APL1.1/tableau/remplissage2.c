ee1746f824843f178846e4eea0a6501546db2a1c
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-10-17 10:42:55 +0200

    Update remplissage2.c

--- APL1.1/tableau/remplissage2.c
+++ APL1.1/tableau/remplissage2.c
@@ -1,30 +1,24 @@
  #include <stdio.h>
  #include <stdlib.h>
 #include <time.h>
-int main(int argc, char const *argv[]){
-	
+int main(int argc, char const *argv[]){	
 	int tab [10], i, a;
 	srand(time(NULL));
-
 	for (i=0; i<10; i++){
 		printf("+-----");
 	}
 	printf("+\n");
 	printf("|");
-
-
 	for(i=0; i<10; i++){
 		a = rand()%101 -50;
 		tab[i]= a;
 		printf("%3d | ", tab[i] );
 	}
 	printf("\n");
-
 	for (i=0; i<10; i++){
 		printf("+-----");
 	}
 	printf("+\n");
 	printf("|");
-
 	return EXIT_SUCCESS;
 }
