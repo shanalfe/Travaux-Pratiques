f0a7c4ec3fdcabb6dc77c16b436d134ea55d942b
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-10-17 10:43:47 +0200

    Update maximum2.c

--- APL1.1/tableau/maximum2.c
+++ APL1.1/tableau/maximum2.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main(int argc, char const *argv[]){
-	
+int main(int argc, char const *argv[]){	
 	int tab [10], i, a, g = 0, ig = 0, j;
 	srand(time(NULL));
-
 	for (i=0; i<10; i++){
 		printf("+-----");
 	}
 	printf("+\n");
 	printf("| ");
-
 	for(i=0; i<10; i++){
 		a = rand()%101 -50;
 		tab[i]= a;
 		printf("%3d | ", tab[i] );
-
 		if (tab[i] > g){
 			g = tab[i];
 			ig = i;
 		}
 	}
 	printf("\n");
-
 	for (i=0; i<10; i++){
 		printf("+-----");
 	}
@@ -32,10 +27,8 @@ int main(int argc, char const *argv[]){
 		for (j=0; j<ig; j++){
 		printf("      ");
 	}
-	printf("^\n");
-	
+	printf("^\n");	
 	printf("le plus grand : %d \n", g);
 	printf("la case : %d \n", ig);
-
 	return EXIT_SUCCESS;
 }
