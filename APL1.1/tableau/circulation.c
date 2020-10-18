eb632c30485fbd0388bd509d1bebfac96f8ac43b
shana <shanoulefevre@orange.fr>
2020-10-18 12:08:14 +0200

    fin circulation

--- APL1.1/tableau/circulation.c
+++ APL1.1/tableau/circulation.c
@@ -1,56 +1,54 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #define  A  4
 #define  B  6
 
-int main(int argc, char const *argv[]){	
-	int tab [10], i, a, tabc[10];
-	srand(time(NULL));
-	for (i=0; i<10; i++){
-		a = rand()%101 -50;
-		tab[i]= a;
-	}
-	for (i=0; i<10; i++){
-		printf("+-----");
-	}
-	printf("+\n");
-	printf("| ");
-	for(i=0; i<10; i++){	
-		printf("%3d | ", tab[i] );
-	}
-	printf("\n");
-	for (i=0; i<10; i++){
-		printf("+-----");
-	}
-	printf("+\n");
-	printf("|");
-	// deuieme tableau
-	for (i=0; i<10; i++){
-		printf("+-----");
-	}
-	printf("+\n");
-	printf("| ");
-	
-	//circulation
-	
-	for(i=0; i<10; i++){		
-		if ( tab[i] < 7){
-			tabc[i] = tab[i + A];
-		} else {
-			tabc[i] = tab[i - B];
-		}
-	}
-	
-	for (i=0; i<10; i++){
-		printf("%3d | ", tabc[i] );
-	}
-	
-	printf("\n");
-	for (i=0; i<10; i++){
-		printf("+-----");
-	}
-	printf("+\n");
-	printf("|");
-	return EXIT_SUCCESS;
+int main(int argc, char const *argv[]){    
+    int tab [10], i, a, tabc[10];
+    srand(time(NULL));
+    for (i=0; i<10; i++){
+        a = rand()%101 -50;
+        tab[i]= a;
+    }
+    for (i=0; i<10; i++){
+        printf("+-----");
+    }
+    printf("+\n");
+    printf("| ");
+    for(i=0; i<10; i++){    
+        printf("%3d | ", tab[i] );
+    }
+    printf("\n");
+    for (i=0; i<10; i++){
+        printf("+-----");
+    }
+    printf("+\n");
+   
+
+    // deuieme tableau
+    for (i=0; i<10; i++){
+        printf("+-----");
+    }
+    printf("+\n");
+    printf("| ");
+    
+    //circulation
+    
+    for(i=0; i<10; i++){        
+        tabc[(i+A)%10] = tab[i];
+    }
+    
+    for (i=0; i<10; i++){
+        printf("%3d | ", tabc[i] );
+    }
+    
+    printf("\n");
+    for (i=0; i<10; i++){
+        printf("+-----");
+    }
+    printf("+\n");
+    printf("|");
+    return EXIT_SUCCESS;
 }
