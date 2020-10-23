40912ad55f255389a67f385eb4b468c30bcea396
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-10-23 16:37:39 +0200

    Create rongeur.c

---
+++ APL1.1/ControleAPL1.1/rongeur.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ #include <stdlib.h>
+int main(int argc, char const *argv[]){
+	
+	printf("\t \t  (\\_/) \n");
+	printf("\t  .-\"\"-.-.-' a\\\n");
+	printf("\t /  \\      _.--'\n");
+	printf("\t(\\  /_---\\_\\_\n");
+	printf("\t `'-.\n");
+	printf("\t  ,__)\n");
+
+	return EXIT_SUCCESS;
+}
