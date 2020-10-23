cec24fa68d162ac6a07e959c752b3f0ea5dd7299
shanalfe <69315804+shanalfe@users.noreply.github.com>
2020-10-23 16:38:07 +0200

    Create regle.c

---
+++ APL1.1/ControleAPL1.1/regle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ #include <stdlib.h>
+int main(int argc, char const *argv[]){
+
+	int l, i =0, j=0, n=0, m=0;
+
+	printf("Entrez la longueur désirée (entre 1 et 9) : ");
+	scanf("%d", &l);
+
+	if( (l>=1) && (l<=9)){
+			for (i=0; i<l; i++){
+				for(j=0; j<10; j++){
+					printf("|");
+			}		
+			
+	}
+	printf("|");
+	printf("\n");
+		printf("0");
+
+	for(m=0; m<l; m++){				
+				n=n+1;								
+				printf("%10d",n );				
+			}	
+	printf("\n");
+	} else{
+		printf("Erreur\n");
+		return EXIT_FAILURE;
+	}
+	
+
+	return EXIT_SUCCESS;
+}
