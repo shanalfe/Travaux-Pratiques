171641296038e4657be6acb797c9a90af9c5734d
Shana <69315804+shanalfe@users.noreply.github.com>
2020-11-13 23:16:17 +0100

    Create triangle.c

---
+++ APL1.1/fonction/exos/triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void Triangle (int t){
+    int i =0, j=0;
+    for (i=0; i)
+
+}
+
+
+
+
+int main(int argc, char const *argv[]){
+    printf ("Taille : ");
+    scanf ("%d", &t);
+    
+    return 0;
+}
