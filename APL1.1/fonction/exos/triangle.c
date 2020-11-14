ff59d5459d6d082ad0c6a755c8697f3ac1ea2fc2
Shana <69315804+shanalfe@users.noreply.github.com>
2020-11-14 13:45:30 +0100

    Update triangle.c

--- APL1.1/fonction/exos/triangle.c
+++ APL1.1/fonction/exos/triangle.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 void Triangle (int t){
     int i =0, j=0;
-    for (i=0; i)
-
+    for (i=0; i < t; i++){
+        for (j=0; j<i+1; j++){
+            printf ("*");
+        }printf ("\n");
+    }
 }
 
-
-
-
 int main(int argc, char const *argv[]){
+    int t;
     printf ("Taille : ");
     scanf ("%d", &t);
-    
+    Triangle (t);    
     return 0;
 }
