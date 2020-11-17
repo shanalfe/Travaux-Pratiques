38691dbea300d180ae65566116e9f15e67c67b3b
Shana <69315804+shanalfe@users.noreply.github.com>
2020-11-17 21:13:55 +0100

    Create boucle_diff.c

---
+++ APL1.1/ProgrammeTest/boucle_diff.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+    
+    int t [8]= {1,3,5,7,11,13,17,19};
+    /*avec la boucle for*/
+    int i;
+    for (i=0; i<8; i=i+1){
+        printf ("  %d", t[i]);
+        i=i+1;
+    }
+
+    printf("\n");
+
+    /*avec la boucle while*/
+   int j =0;
+    while (j<8){        
+            printf ("  %d", t[j]);
+             j=j+2;         
+    }printf ("\n");
+
+    /*avec la boucle do while*/
+    int k = 0;
+    do {
+        printf ("  %d", t [k]);
+        k=k+2;
+    } while ( k<8 );
+
+    return 0;
+}
