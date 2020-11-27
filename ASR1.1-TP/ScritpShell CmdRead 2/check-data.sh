eedc34bae0276e1c6a33855e6bff0ac6ad76c022
Shana <69315804+shanalfe@users.noreply.github.com>
2020-11-27 10:57:09 +0000

    Create check-data.sh

---
+++ ASR1.1-TP/ScritpShell CmdRead 2/check-data.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [[$# -ne 3]]
+then
+	echo -n "Usage : $0"
+	echo "<STRINGS_REPRESENTATIONS_FILE><radix> <REPORTS_FILE>"
+	echo "<radix> <REPORTS_FILE>"
+	exit
+fi
+
+
+if [[ ! ($2 -ge 2) && ($2 -lt 36) ]]
+then
+	echo "The radix is a decimal between 2 and 36"
+fi
+
+
+if [[ ! -f $1 ]]
+then
+	echo "none.dat does not exist or is not regular."
+	exit
+fi
+
+
+if [[-f "$3"]]
+then
+	echo -n "File $3 exists. Owervrite ? Tes/No :"
+	read confirm
+	if [[confirm != Yes]]
+	then
+		echo "Opération annulée"
+		exit
+	fi
+fi
