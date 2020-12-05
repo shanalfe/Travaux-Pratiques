4ada12e6aac6aaf5836811b55bcc8b745683d8ab
Shana <69315804+shanalfe@users.noreply.github.com>
2020-12-05 17:38:18 +0100

    Update reccord2.c

--- APL1.2/fichier/reccord2.c
+++ APL1.2/fichier/reccord2.c
@@ -3,27 +3,19 @@
 
 int main(int argc, char const *argv[]){
 	int* s;
-	char n [3];
-	int i=0;
-
-
+	char n [3];	
 	FILE* flux;
-
 	flux = fopen ("top10", "r");
-	if (flux){
-	
-	while (! feof (flux) ){		
+	if (flux){	
+		while (! feof (flux) ){		
 			fread (&s, sizeof(int),1, flux );
 			fread (&n, sizeof (char), 3, flux);	
 			printf("%s : %09i \n",n, s );
 		}
-
 	fclose (flux);
-
 	}else {
 		perror ("The file doesn't exist !");
 		return EXIT_FAILURE;
-	}	
-	
+	}		
 	return EXIT_SUCCESS;
 }
