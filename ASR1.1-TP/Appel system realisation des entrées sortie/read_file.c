3edc455b4433057d39018aada9da5b59793e84b1
Shana <69315804+shanalfe@users.noreply.github.com>
2020-12-05 16:06:31 +0100

    Update read_file.c

--- ASR1.1-TP/Appel system realisation des entrées sortie/read_file.c
+++ ASR1.1-TP/Appel system realisation des entrées sortie/read_file.c
@@ -1,33 +1,25 @@
 #include <unistd.h>
 #include <fcntl.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 
 #define TRANCHE 256
 
-
 int main(int argc, char const *argv[]){
-
 	char tampon [TRANCHE];
 	int descripteur;
 	ssize_t lus, ecrit;
-
 	if (argc !=2){
 		printf("Usage : %s <file_name> \n", argv [0]);
 		return EXIT_FAILURE;
 	}
-
 	descripteur = open (argv[1], O_RDONLY);
-
 	/*Echec de l'ouverture*/
 	if (descripteur == -1){
 		perror ("Error oppening file :");
 		return EXIT_FAILURE;
 	}
-
 	do {
-
 		lus = read (descripteur, tampon, TRANCHE);
 		if (lus == -1){
 			perror ("Error macking file :");
@@ -40,9 +32,6 @@ int main(int argc, char const *argv[]){
 		}
 
 	}while (lus== TRANCHE);
-
 	close(descripteur); // = exit (EXIT_SUCCESS);
-
-
 	return EXIT_SUCCESS;
 }
