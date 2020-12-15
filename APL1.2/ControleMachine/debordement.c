0934fb0f5649c4b9f07ca3ad17357c7bf3a871b8
Shana <69315804+shanalfe@users.noreply.github.com>
2020-12-15 14:09:42 +0100

    Create debordement.c

---
+++ APL1.2/ControleMachine/debordement.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char * const argv [] {
+  int l = 1;
+  int i = 1;
+  
+  while ( ( (l*i)/l ) == i) {
+    l = l*i;
+    i++;
+ }
+ i--;
+ printf ("valeur de i = %d, valeur de %d! = %lu, %lu, %lu \n", i, i, l, f(i));
+ 
+ return EXIT_SUCCESS;
+ 
+ }
