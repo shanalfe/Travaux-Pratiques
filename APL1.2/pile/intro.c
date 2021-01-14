8668aa4304ecb76d3bb62e6e18551f0c1a4ab56a
shana <shanoulefevre@orange.fr>
2021-01-14 22:30:51 +0100

    aj

---
+++ APL1.2/pile/intro.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Element Element;
+struct Element
+{
+    int nombre;
+    Element *suivant;
+};
+
+typedef struct Pile Pile;
+struct Pile
+{
+    Element *premier;
+};
+
+
+void empiler(Pile *pile, int nvNombre)
+{
+    Element *nouveau = malloc(sizeof(*nouveau));
+    if (pile == NULL || nouveau == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    nouveau->nombre = nvNombre;
+    nouveau->suivant = pile->premier;
+    pile->premier = nouveau;
+}
+
+int depiler(Pile *pile)
+{
+    if (pile == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    int nombreDepile = 0;
+    Element *elementDepile = pile->premier;
+
+    if (pile != NULL && pile->premier != NULL)
+    {
+        nombreDepile = elementDepile->nombre;
+        pile->premier = elementDepile->suivant;
+        free(elementDepile);
+    }
+
+    return nombreDepile;
+}
+
+
+void afficherPile(Pile *pile)
+{
+    if (pile == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    Element *actuel = pile->premier;
+
+    while (actuel != NULL)
+    {
+        printf("%d\n", actuel->nombre);
+        actuel = actuel->suivant;
+    }
+
+    printf("\n");
+}
+
+
+int main()
+{
+    Pile *maPile = initialiser();
+
+    empiler(maPile, 4);
+    empiler(maPile, 8);
+    empiler(maPile, 15);
+    empiler(maPile, 16);
+    empiler(maPile, 23);
+    empiler(maPile, 42);
+
+    printf("\nEtat de la pile :\n");
+    afficherPile(maPile);
+
+    printf("Je depile %d\n", depiler(maPile));
+    printf("Je depile %d\n", depiler(maPile));
+
+    printf("\nEtat de la pile :\n");
+    afficherPile(maPile);
+
+    return 0;
+}
--- APL1.2/recusivité suite/triangle.c
+++ APL1.2/recusivité suite/triangle.c
@@ -1,26 +1,105 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <graph.h>
-#include <math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<graph.h>
 
+#define X 0
+#define Y 1000
+#define XX 1000
+#define YY 1000
+#define XXX 500
+#define YYY 0
 
-void Rectangle(){
-	RemplirTriangle(100,600,600,600,350,100);
+typedef struct 
+{
+	int x;
+	int y;
+}point;
+
+typedef struct 
+{
+	point a;
+	point b;
+	point c;
+}Triangle;
+
+point Midline(point a, point b)
+{
+	point p;
+	p.x = (a.x + b.x)/2;
+	p.y = (a.y + b.y)/2;
+	return p;
 }
 
+Triangle left(Triangle tri)
+{
+	Triangle triangle;
+	triangle.a = tri.a;
+	triangle.b = Midline(tri.a,tri.b);
+	triangle.c = Midline(tri.a,tri.c);
+	return triangle;
+}
 
+Triangle right(Triangle tri)
+{
+	Triangle triangle;
+	triangle.a = Midline(tri.a,tri.b);
+	triangle.b = tri.b;
+	triangle.c = Midline(tri.b,tri.c);
+	return triangle;
+}
 
+Triangle top(Triangle tri)
+{
+	Triangle triangle;
+	triangle.a = Midline(tri.a,tri.c);
+	triangle.b = Midline(tri.c,tri.b);
+	triangle.c = tri.c;
+	return triangle;
+}
 
-int main(int argc, char const *argv[]){
+void draw(int repeat,Triangle tri) {
+	if (repeat == 0)
+	{
+		couleur c;
+		c=CouleurParNom("black");
+		ChoisirCouleurDessin(c);
+	    RemplirTriangle(tri.a.x,tri.a.y,tri.b.x,tri.b.y,tri.c.x,tri.c.y);
+	}
+	else
+	{
+		draw(repeat-1,left(tri));
+		draw(repeat-1,right(tri));
+		draw(repeat-1,top(tri));
+	}
+}
 
-	InitialiserGraphique ();
+int main(int argc, char * argv[]){
+	int repeat;
+	Triangle tri;
+	tri.a.x=X;
+	tri.a.y=Y;
+	tri.b.x=XX;
+	tri.b.y=YY;
+	tri.c.x=XXX;
+	tri.c.y=YYY;
 
-	CreerFenetre (20,20, 800,800);
-	Rectangle();
+    if (argc != 2)
+    {
+    	fprintf(stderr, "format: %s <nombre de repetition> \n",argv[0]);
+    	return EXIT_FAILURE;
+    }
 
-	Touche();
-	FermerGraphique();
+   	repeat = atoi(argv[1]);
 
+   	printf("%d\n",repeat );
 
-return EXIT_SUCCESS;
+	InitialiserGraphique();
+    CreerFenetre(0,0,1000,1000);
+
+    draw(repeat,tri);
+
+	Touche();	
+    FermerGraphique();
+
+	return EXIT_SUCCESS;
 }
