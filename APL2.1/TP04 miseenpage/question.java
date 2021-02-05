1bca355cb038dcab4a793163b6c10f6b2ef6e5fe
Shana <69315804+shanalfe@users.noreply.github.com>
2021-02-05 09:26:07 +0000

    Update question.java

--- APL2.1/TP04 miseenpage/question.java
+++ APL2.1/TP04 miseenpage/question.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Dimension;
-import javax.swing.JLabel;
+
+/**
+* @version 1.1 
+* @author Shana Lefevre AND Max DUCOUDRE
+*/
 
 public class question {
 	public static void main(String[] args) {
@@ -9,34 +12,36 @@ public class question {
 		// creation de la fenetre
 		JFrame fenetre = new JFrame();
 		fenetre.setSize(400,200);
-		
-		// Creation du panneau
 
-		JPanel panneau = new JPanel();
-		
+		Dimension dim = new Dimension (250, 185);
+		fenetre.setMinimumSize (dim);
+			
+		GridLayout gestionnaire = new GridLayout(3, 1);
+		fenetre.setLayout(gestionnaire);
+					
 		JLabel quest = new JLabel("Aimez-vous les chiens?");
 		//placement vertical du texte
 		quest.setSize(200,400);
-		//permet l'alignement du texte (non modifiable)
 		quest.setHorizontalAlignment(JLabel.CENTER);
 
-		// ajout de la question a la fenetre
-		fenetre.add(quest, BorderLayout.CENTER); 
 
+		JPanel panneau = new JPanel();
 		//création des boutons
 		JButton oui = new JButton("OUI");
 		panneau.add(oui);
-
 		JButton non = new JButton("NON");
 		panneau.add(non);
-
 		JButton nspp = new JButton("NSPP");
 		panneau.add(nspp);
 
+		JPanel panel = new JPanel ();
+		panel.setLayout (new BorderLayout () );
+		panel.add (panneau, BorderLayout.CENTER);
+		panel.add  (quest, BorderLayout.NORTH);
+		fenetre.add (new JLabel ("") );
+		fenetre.add (panel);
 
-		// ajoute du panneau à la fenetre, au sud de la fenetre
-		fenetre.add(panneau, BorderLayout.SOUTH);
-		// affichage de la fenetre
+		
 		fenetre.setVisible(true);
 		
 	}
