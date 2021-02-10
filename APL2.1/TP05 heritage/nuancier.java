c5842cb132ed1fc3f2af3ad6a59961b104068dea
shana <shanoulefevre@orange.fr>
2021-02-10 11:06:15 +0100

    maj

--- APL2.1/TP05 heritage/nuancier.java
+++ APL2.1/TP05 heritage/nuancier.java
@@ -25,10 +25,10 @@ public class nuancier {
 
 	public Component pan () {
 		JPanel panneau = new JPanel ();		
-		panneau.setMinimumSize (new Dimension (150,150));
-		panneau.setMaximumSize (new Dimension (150,150));
-		panneau.setLayout (new BorderLayout ());
-		panneau.setBorder(BorderFactory.createEmptyBorder(5, 2, 5, 2));
+		panneau.setMinimumSize (new Dimension (100,100));
+		panneau.setMaximumSize (new Dimension (100,100));
+		panneau.setLayout (new BorderLayout ()); //par défaut du JPanel : flo donc alors que l'on veut un borderlayout
+		panneau.setBorder(BorderFactory.createEmptyBorder(5, 2, 5, 2)); // aspect bordure
 
 		JLabel h = new JLabel (this.r + "," + this.v + "," + this.b);
 		h.setOpaque (true);
@@ -51,11 +51,10 @@ public class nuancier {
 		JFrame w = new JFrame ("Nuancier");
 		w.setSize (500,500);
 		w.setLocation (100,100);
-
 		w.setLayout (new GridLayout (1,args.length));
 
 		for (i = 0; i< args.length; i++){
-			w.add (new nuancier ( args[i]).pan ());
+			w.add (new nuancier ( args[i]).pan () );
 		}
 
 		w.setVisible (true);
