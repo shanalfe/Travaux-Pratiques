05a32c19e6454b488ec83bb7c32b2bbaff1cc679
Shana <69315804+shanalfe@users.noreply.github.com>
2021-02-10 22:15:14 +0100

    Update nuancier.java

--- APL2.1/TP05 heritage/nuancier.java
+++ APL2.1/TP05 heritage/nuancier.java
@@ -14,9 +14,13 @@ public class nuancier {
 	private int v;
 	private Color c;
 
-
+	/*--- Methode ---*/
 	public nuancier (String h){
+		//Convertit a Stringen entier et renvoie l'opaque spécifié Color.
+		//Cette méthode gère les formats de chaîne utilisés pour représenter les nombres octaux et hexadécimaux.
 		this.c = c.decode (h);
+		
+		//Renvoie le composant rouge dans la plage 0-255 dans l'espace sRGB par défaut.
 		this.r = c.getRed();
 		this.b = c.getBlue();
 		this.v = c.getGreen();
@@ -30,13 +34,15 @@ public class nuancier {
 		panneau.setLayout (new BorderLayout ()); //par défaut du JPanel : flo donc alors que l'on veut un borderlayout
 		panneau.setBorder(BorderFactory.createEmptyBorder(5, 2, 5, 2)); // aspect bordure
 
-		JLabel h = new JLabel (this.r + "," + this.v + "," + this.b);
+		/*--- Inscription de la couleur ---*/
+		JLabel h = new JLabel (this.r + "," + this.v + "," + this.b); // Affichage
 		h.setOpaque (true);
 		h.setBackground (Color.BLACK);
 		h.setForeground (Color.WHITE);
 		h.setHorizontalAlignment (JLabel.CENTER);
 		panneau.add (h, BorderLayout.NORTH);
-
+		
+		/*--- Carré de couleur ---*/
 		JLabel color = new JLabel ();
 		color.setBackground (c);
 		color.setOpaque (true);
@@ -48,7 +54,7 @@ public class nuancier {
 
 	public static void main(String[] args) {	
 		int i;
-		JFrame w = new JFrame ("Nuancier");
+		JFrame w = new JFrame ("Nuancier"); // Appel de la classe Nuancier
 		w.setSize (500,500);
 		w.setLocation (100,100);
 		w.setLayout (new GridLayout (1,args.length));
