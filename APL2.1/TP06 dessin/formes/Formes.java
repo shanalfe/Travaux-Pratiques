fd6ca2c02b9c5f8d728d6055910c89d8a1298a0f
Shana <69315804+shanalfe@users.noreply.github.com>
2021-02-12 08:32:11 +0000

    Add files via upload

--- APL2.1/TP06 dessin/formes/Formes.java
+++ APL2.1/TP06 dessin/formes/Formes.java
@@ -1,24 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+* @version 2.1 12 février 2021
+* @author Shana Lefevre
+*/
+
 
 public class Formes {
 
 
 	//pas besoin d'un import car carré est dans le même repertoire
 
-
 	public static void main(String[] args) {
-		
-
 		
 		JFrame fenetre = new JFrame ();
 		fenetre.setSize(800,450);
 		fenetre.setLocation(0,0);
+		fenetre.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		
 
 
 		//création de la classe carré ; first est la variable
-		carre first = new carre();
+		Carre first = new Carre();
 
 		
 		//ajout de la classe carré à la fenetre
