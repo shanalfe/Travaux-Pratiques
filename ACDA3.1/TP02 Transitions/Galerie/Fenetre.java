dc1cbc61a4ad0ae489c6d9cd1ee152b8eb85892b
shana <shanoulefevre@orange.fr>
2021-11-17 18:57:19 +0100

    Vérification, OK

--- ACDA3.1/TP02 Transitions/Galerie/Fenetre.java
+++ ACDA3.1/TP02 Transitions/Galerie/Fenetre.java
@@ -24,7 +24,11 @@ public class Fenetre extends JFrame {
 		JLabel p2 = new JLabel (new ImageIcon ("images/loup2.jpg"));
 		picture.add(p2);
 		JLabel p3 = new JLabel (new ImageIcon ("images/loup3.jpg"));
-		picture.add(p3);
+		picture.add(p3);		
+		JLabel p4 = new JLabel (new ImageIcon ("images/ej.jpg"));
+		picture.add(p4);
+		JLabel p5 = new JLabel (new ImageIcon ("images/montagne.jpg"));
+		picture.add(p5);
 
 		Event event = new Event (diapo, picture);
 		picture.addMouseListener(event);
