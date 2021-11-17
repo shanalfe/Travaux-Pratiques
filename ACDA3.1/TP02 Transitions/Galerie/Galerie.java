8bafec20c5b45af220193b0e6d1da16529c34299
shana <shanoulefevre@orange.fr>
2021-11-17 17:23:31 +0100

    ENFIN

---
+++ ACDA3.1/TP02 Transitions/Galerie/Galerie.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Galerie {
+	
+	JLabel p1 = new JLabel (new ImageIcon (""));
+ 		
+
+
+
+
+}
---
+++ ACDA3.1/TP02 Transitions/Galerie/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Fenetre fenetre = new Fenetre();
+		fenetre.setVisible(true);
+
+	}
+
+}
