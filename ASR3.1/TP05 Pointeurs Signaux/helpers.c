8f53c410b5c0508311344d7e4d8a46a81abf8e05
shana <shanoulefevre@orange.fr>
2021-12-07 17:35:26 +0100

    ajout new

---
+++ ASR3.1/TP05 Pointeurs Signaux/helpers.c
@@ -0,0 +1,16 @@
+#include "helpers.h"
+#include <signal.h>
+#include <time.h>
+int set_signal_handler(int signo, void (*handler)(int)) {
+    struct sigaction sa;
+    sa.sa_handler = handler;    // call `handler` on signal
+    sigemptyset(&sa.sa_mask);   // don't block other signals in handler
+    sa.sa_flags = SA_RESTART;            //  restart system calls
+    return sigaction(signo, &sa, NULL);
+}
+
+double tstamp(void) {                                                                                    
+	struct timespec tv;
+	clock_gettime(CLOCK_REALTIME, &tv);
+	return tv.tv_sec + tv.tv_nsec * 1.0e-9;
+}
---
+++ ASR3.1/TP05 Pointeurs Signaux/helpers.h
@@ -0,0 +1,6 @@
+#ifndef _HELPERS_H
+#define _HELPERS_H
+int set_signal_handler(int signo, void (*handler)(int));
+double tstamp(void);
+
+#endif
