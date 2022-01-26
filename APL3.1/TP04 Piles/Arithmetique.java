d9a90c32b532093fdd609bf43310f55c2441203b
shana <shanoulefevre@orange.fr>
2022-01-26 22:14:50 +0100

    enfin

--- APL3.1/TP04 Piles/Arithmetique.java
+++ APL3.1/TP04 Piles/Arithmetique.java
@@ -1,50 +1,70 @@
 import java.util.*;
-import java.lang.Object.*;
+
 
 
 public class Arithmetique {
 
 	public static void main (String[] args) {
+		
 		int i = 1;
 				
 		
 		//Gestion des erreurs
 		if (args.length == 1) {
-			System.out.println ("Error");
+			System.out.println ("Il manque des arguments");
 		
+
 		}
 
-		Deque<Integer> deque = new ArrayDeque<>();
+		Deque<String> liste = new ArrayDeque<>();
 		
-		for (i=1; i<args.length; i++) {
-			
-			deque.push(Integer.parseInt(args[i]));
+		for (i = 0; i<args.length; i++) {	
 
+			liste.push(args[i]);	
+
+			System.out.println(liste);
+				
 			if (args[i].equals("+") ) {
 
-				deque.pop();
-				deque.pop();
+				liste.pop(); // On enlève le signe que l'on a ajout
+				int nb1 = Integer.parseInt(liste.pop());
+				int nb2 = Integer.parseInt(liste.pop());
+				System.out.println(nb2+" "+ nb1);
+
+				String res = String.valueOf(nb1 + nb2);
+				
+				liste.push(res);
+				
 			
-			}
-
-			if (args[i].equals("-") ) {
-				System.out.println ("PLus");
+			} else if (args[i].equals("-") ) {
+				liste.pop();
+				int nb1 = Integer.parseInt(liste.pop());
+				int nb2 = Integer.parseInt(liste.pop());
+				
+				String res = String.valueOf(nb2 - nb1);				
+				liste.push(res);
 			
-			}
-
-			if (args[i].equals("/") ) {
-				System.out.println ("PLus");
+			} else if (args[i].equals("/") ) {
+				liste.pop();
+				int nb1 = Integer.parseInt(liste.pop());
+				int nb2 = Integer.parseInt(liste.pop());
+				String res = String.valueOf(nb1 / nb2);				
+				liste.push(res);
+				
 			
-			}
-
-			if (args[i].equals("x") ) {
-				System.out.println ("PLus");
+			}else if (args[i].equals("x") ) {
+				liste.pop();
+				int nb1 = Integer.parseInt(liste.pop());
+				int nb2 = Integer.parseInt(liste.pop());
+				String res = String.valueOf(nb1 * nb2);				
+				liste.push(res);
+				
 			
-			}
-
-
+			} 
 
 		}
+		
+		System.out.println ("Pile finale : " + liste);
 
 	}
 	
