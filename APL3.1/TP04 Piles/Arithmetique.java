7834bde93692370b4cbf701de7a8bf5a80ffa90c
shana <shanoulefevre@orange.fr>
2022-01-27 10:14:27 +0100

    correction erreur

--- APL3.1/TP04 Piles/Arithmetique.java
+++ APL3.1/TP04 Piles/Arithmetique.java
@@ -1,73 +1,59 @@
 import java.util.*;
 
 
-
+/**
+* Classe Arithmetique qui permet le calcul des piles
+*/
 public class Arithmetique {
+
 
 	public static void main (String[] args) {
 		
-		int i = 1;
-				
+		int i = 1;				
 		
-		//Gestion des erreurs
-		if (args.length == 1) {
-			System.out.println ("Il manque des arguments");
-		
+		if (args.length == 1) { // Gestion des erreurs
+			System.out.println ("Il manque des arguments !");
 
+		} else {
+
+			Deque<String> liste = new ArrayDeque<>(); // Création de la pile de type String
+			
+			for (i = 0; i < args.length; i ++) {	
+
+				liste.push(args[i]); // Ajout de chaque argument dans la pile
+
+				System.out.println("Pile : " + liste);
+					
+				if (args[i].equals("+") ) {
+
+					liste.pop(); // On enlève le signe que l'on a ajout
+					int nb1 = Integer.parseInt(liste.pop()); // On enlève et on converti en int
+					int nb2 = Integer.parseInt(liste.pop()); // On enlève et on converti en int					
+					liste.push(String.valueOf(nb1 + nb2)); // On converti en String et on ajoute dans la pile
+				
+				} else if (args[i].equals("-") ) {
+					liste.pop();
+					int nb1 = Integer.parseInt(liste.pop());
+					int nb2 = Integer.parseInt(liste.pop());			
+					liste.push(String.valueOf(nb2 - nb1));
+				
+				} else if (args[i].equals("/") ) {
+					liste.pop();
+					int nb1 = Integer.parseInt(liste.pop());
+					int nb2 = Integer.parseInt(liste.pop());			
+					liste.push(String.valueOf(nb2 / nb1));
+					
+				} else if (args[i].equals("x") ) {
+					liste.pop();
+					int nb1 = Integer.parseInt(liste.pop());
+					int nb2 = Integer.parseInt(liste.pop());			
+					liste.push(String.valueOf(nb1 * nb2));
+				} 
+
+			}
+			
+			System.out.println ("total : " + liste);
 		}
-
-		Deque<String> liste = new ArrayDeque<>();
-		
-		for (i = 0; i<args.length; i++) {	
-
-			liste.push(args[i]);	
-
-			System.out.println(liste);
-				
-			if (args[i].equals("+") ) {
-
-				liste.pop(); // On enlève le signe que l'on a ajout
-				int nb1 = Integer.parseInt(liste.pop());
-				int nb2 = Integer.parseInt(liste.pop());
-				System.out.println(nb2+" "+ nb1);
-
-				String res = String.valueOf(nb1 + nb2);
-				
-				liste.push(res);
-				
-			
-			} else if (args[i].equals("-") ) {
-				liste.pop();
-				int nb1 = Integer.parseInt(liste.pop());
-				int nb2 = Integer.parseInt(liste.pop());
-				
-				String res = String.valueOf(nb2 - nb1);				
-				liste.push(res);
-			
-			} else if (args[i].equals("/") ) {
-				liste.pop();
-				int nb1 = Integer.parseInt(liste.pop());
-				int nb2 = Integer.parseInt(liste.pop());
-				String res = String.valueOf(nb2 / nb1);				
-				liste.push(res);
-				
-			
-			}else if (args[i].equals("x") ) {
-				liste.pop();
-				int nb1 = Integer.parseInt(liste.pop());
-				int nb2 = Integer.parseInt(liste.pop());
-				String res = String.valueOf(nb1 * nb2);				
-				liste.push(res);
-				
-			
-			} 
-
-		}
-		
-		System.out.println ("Pile finale : " + liste);
-
 	}
-	
-
 
 }
