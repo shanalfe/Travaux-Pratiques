f094d76cd9e22251635c1cc96c3104d8c5598e3a
Shana <69315804+shanalfe@users.noreply.github.com>
2022-02-18 01:06:53 +0100

    Delete APL1.1/organisation code directory

--- APL1.1/organisation code/exo1/lire.h
+++
@@ -1,8 +0,0 @@
-/* TP 19 Exercice 1 : fichier lire.h */
-
-#ifndef LIRE_H
-#define LIRE_H
-
-void lire(char*, int);
-
-#endif /* LIRE_H */
--- APL1.1/organisation code/exo1/option.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPTION_H
-#define OPTION_H
-
-
-typedef enum {AJOUTER, AFFICHER, SORTIR} options;
-
-options saisir_option();
-
-#endif
