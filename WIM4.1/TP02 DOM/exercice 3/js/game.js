c25980ff6fffa1febd13c5c5834b54b60e9e0539
shana <shanoulefevre@orange.fr>
2022-04-12 12:31:40 +0100

    avancée'

--- WIM4.1/TP02 DOM/exercice 3/js/game.js
+++ WIM4.1/TP02 DOM/exercice 3/js/game.js
@@ -1,1 +1,28 @@
 // TODO	
+
+let btn = document.getElementsByTagName("button")[0]
+let img = document.getElementsByTagName("img")[0]
+let p = document.getElementsByTagName("p")[0]
+let figure = document.getElementsByTagName("figure")[0]
+let span = document.getElementsByTagName("figure")[0].lastElementChild
+
+
+function Fin() {
+	btn.removeEventListener("click", Score, false)
+}
+
+/*Fonction de l'évenement bouton start*/
+function ReponseClick (e) {
+	btn.classList.add("hidden") //permet de cacher le bouton
+	p.classList.remove("hidden") //permet d'afficher la mamie
+	setTimeout(Fin, 2000)
+}
+
+/*Crétion de l'évenement bouton start*/
+btn.addEventListener("click", ReponseClick, false)
+
+
+
+function Score () {
+	span.innerText = Number(span.innerText) + 1
+}
