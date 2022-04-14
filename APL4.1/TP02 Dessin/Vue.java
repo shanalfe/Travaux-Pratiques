55d72d06e054b5b84f91cce4818a2502827bab4d
Shana <69315804+shanalfe@users.noreply.github.com>
2022-04-14 11:36:46 +0200

    Update Vue.java

--- APL4.1/TP02 Dessin/Vue.java
+++ APL4.1/TP02 Dessin/Vue.java
@@ -1,6 +1,9 @@
 package com.example.dessin;
-
+/*Imports */
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -10,22 +13,38 @@ import android.view.contentcapture.ContentCaptureSession;
 import android.graphics.Paint;
 
 
-
+/*Class Vue */
 public class Vue extends View {
 
+    /*Déclaration des variables*/
     private Paint paint;
+    private Resources res;
+    private Bitmap img;
 
+    /*Constructeur*/
    public Vue (Context context, AttributeSet attrs) {
       super(context, attrs);
        this.paint = new Paint(Paint.FAKE_BOLD_TEXT_FLAG);
+       this.res = this.getResources();
+       this.img = BitmapFactory.decodeResource(this.res, R.drawable.cercles);
 
    }
 
+   /*Méthode création des déssins*/
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        paint.setColor(Color.BLUE);
         canvas.drawRect(100, 70, 300, 200, this.paint);
+
+        paint.setColor(Color.YELLOW);
+        canvas.drawCircle(600, 100, 35, this.paint);
+
+        canvas.drawBitmap(this.img, 100,300, this.paint);
+
         paint.setColor(Color.BLACK);
+        canvas.drawText("Ceci est un test", 100,460, this.paint);
+        paint.setTextSize(60);
 
 
 
