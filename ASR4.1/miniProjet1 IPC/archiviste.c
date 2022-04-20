85d7783f371ffbf6ccfcee47b47c527ac4eb5e8d
shana <shanoulefevre@orange.fr>
2022-04-20 21:36:40 +0100

    premier jet

---
+++ ASR4.1/miniProjet1 IPC/archiviste.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <signal.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+
+
+int main(int argc, char const *argv[]){
+	
+	return 0;
+}
--- ASR4.1/miniProjet1 IPC/main.c
+++ ASR4.1/miniProjet1 IPC/main.c
@@ -1,11 +1,27 @@
+/*
+	Main : créer les archivites et les journalistes.
+	journaliste : segment de mémoire partagée de taille bornée
+
+	archiviste : file de message
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <signal.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+
 #include "type.h"
+
+/*Déclaration des variables*/
+int id_file;
 
 
 /*Nettoyage*/
-void arret(int s){
-	
+void arret(int s){	
 	msgctl(id_file, IPC_RMID, NULL);
 }
 
@@ -28,13 +44,14 @@ int main(int argc, char const *argv[]){
 	/*Déclaration des variables*/
 	int nb_archivistes = 0,
 		nb_themes = 0,
-		id_file; 
+		i= 1; 
 	key_t cle_fichier;
 	pid_t pid = getpid();
+	pid_t p;
 	ssize_t nb_lus;
 
 
-	cle_fichier = ftok(CLE_FICHIER, 'a'); //calcul de la clé de la fe
+	cle_fichier = ftok(FICHIER_CLE, 'a'); //calcul de la clé de la fe
 	assert(cle_fichier != -1);
 
 	/*Condition arguments ligne de commande*/
@@ -54,8 +71,18 @@ int main(int argc, char const *argv[]){
 			return EXIT_FAILURE;
 		} else {
 			printf("nb_archivistes : %d, nb_themes :%d\n", nb_archivistes, nb_themes);
-			assert(set_signal_handler(SIGNINT, arret) == 0);
+			assert(set_signal_handler(SIGINT, arret) == 0);
 
+			/*Création archivistes et journalistes*/
+			for (i = 1; i<= nb_archivistes; i ++) {
+				p = fork();
+				assert(p != -1);
+				if (p == 0)  {
+					snprintf(nb_archivistes,sizeof(nb_archivistes),"%d",i);
+				execl("archiviste","archiviste",buf,NULL);
+				assert(0);
+				}
+			}
 			
 		}
 	}
